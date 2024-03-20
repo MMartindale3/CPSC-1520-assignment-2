007cc7f0ac40635069f74118211eba5f84e92071
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-03-19 22:18:17 -0600

    Currently loads albums.json into console

--- js/index.js
+++ js/index.js
@@ -8,3 +8,32 @@
   <td>NUMBER OF RATINGS HERE</td>
 </tr> 
 */
+
+let albumsStore
+async function appInit() {
+  const response = await fetch("public/albums.json");
+  const albums = await response.json();
+  const albumsArray = albums;
+  const albumsStore = [...albumsArray];
+
+  render(albums, document.querySelector("tbody"))
+}
+
+function render(data, container) {
+  console.table(data)
+  // data.forEach(element => {
+  //   const template = `
+  //   <tr>
+  //     <td>${albumsStore.album}</td>
+  //     <td>${artistName}</td>
+  //     <td>${releaseDate}</td>
+  //     <td>${genres}</td>
+  //     <td>${averageRating}</td>
+  //     <td>${numberRatings}</td>
+  //   </tr>
+  //   `
+  //   container.insertAdjacentHTML("beforeend", template)
+  // });
+}
+appInit();
+
